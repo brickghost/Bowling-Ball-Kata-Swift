264ed277479305b03cf2e103c42a9be889b9cce6
Mr. Angry Pants <abricker@mac.com>
2019-01-29 13:22:07 -0600

    Test one spare

--- SwiftBowling.playground/Contents.swift
+++ SwiftBowling.playground/Contents.swift
@@ -1,15 +1,44 @@
 import XCTest
 
 class BowlingGame {
-    var rolls = [Int]()
-    var currentFrame = 1
+    var frames = [Int: [Int]]()
+    var currentFrame = 0
+    var rolls = 0
 
     func roll(pins: Int) {
-        rolls.append(pins)
+        
+        if(rolls % 2 == 0) {
+            currentFrame += 1
+            frames[currentFrame] = [Int]()
+        }
+        
+        if(currentFrame > 1) {
+            maybeScoreSpare(prevFrame: currentFrame - 1, pins: pins)
+        }
+        
+        if(frames[currentFrame]?.count == 0) {
+            frames[currentFrame]?.append(pins)
+        } else if(frames[currentFrame]?.count == 1) {
+            frames[currentFrame]?.append(pins)
+        }
+        
+        rolls += 1
+    }
+    
+    func maybeScoreSpare(prevFrame: Int, pins: Int) {
+        if(frames[prevFrame]?.reduce(0) { $0 + $1 } == 10) {
+            frames[prevFrame]?[1] += pins
+        }
     }
 
     func score() -> Int{
-        return rolls.reduce(0, +)
+        var score = 0
+        
+        for key in 1...10 {
+            score += frames[key]?.reduce(0) { $0 + $1 } ?? 0
+        }
+        
+        return score
     }
 }
 
@@ -45,6 +74,21 @@ class BowlingGameTests: XCTestCase {
         }
         
         XCTAssertEqual(game?.score(), count)
+    }
+
+    func testOneSpare() {
+        game?.roll(pins: 6)
+        game?.roll(pins: 4)
+        game?.roll(pins: 7)
+
+        var count: Int = 0
+        for _ in 1...17 {
+            let randNum = Int.random(in: 1..<5)
+            game?.roll(pins: randNum)
+            count = count + randNum
+        }
+
+        XCTAssertEqual(game?.score(), count + 24)
     }
 }
 
