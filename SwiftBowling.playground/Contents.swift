1ce3043bf76954324854ab134bb1e30e26d02b3c
Mr. Angry Pants <abricker@mac.com>
2019-01-29 06:23:20 -0600

    Test for gutter game

--- SwiftBowling.playground/Contents.swift
+++ SwiftBowling.playground/Contents.swift
@@ -1,12 +1,22 @@
 import XCTest
 
-struct BowlingGame {
+class BowlingGame {
+    var frames = [Int: [Int]]()
 
+    func roll(pins: Int) {
+        
+    }
+
+    func score() -> Int{
+        return 0
+    }
 }
 
 class BowlingGameTests: XCTestCase {
+    var game: BowlingGame?
     
     override func setUp() {
+        game = BowlingGame()
         super.setUp()
     }
     override func tearDown() {
@@ -14,7 +24,15 @@ class BowlingGameTests: XCTestCase {
     }
     
     func testBowlingGame() {
-        XCTAssertNotNil(BowlingGame())
+        XCTAssertNotNil(game)
+    }
+
+    func testGutterGame() {
+        for _ in 1...20 {
+            game?.roll(pins: 0)
+        }
+
+        XCTAssertEqual(game?.score(), 0)
     }
 }
 
