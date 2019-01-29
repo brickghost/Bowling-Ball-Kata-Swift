a5d3c46b7196120567c53c12a63a3e76f630014d
Mr. Angry Pants <abricker@mac.com>
2019-01-29 07:18:21 -0600

    Test no strikes or spares

--- SwiftBowling.playground/Contents.swift
+++ SwiftBowling.playground/Contents.swift
@@ -1,14 +1,15 @@
 import XCTest
 
 class BowlingGame {
-    var frames = [Int: [Int]]()
+    var rolls = [Int]()
+    var currentFrame = 1
 
     func roll(pins: Int) {
-        
+        rolls.append(pins)
     }
 
     func score() -> Int{
-        return 0
+        return rolls.reduce(0, +)
     }
 }
 
@@ -33,6 +34,17 @@ class BowlingGameTests: XCTestCase {
         }
 
         XCTAssertEqual(game?.score(), 0)
+    }
+    
+    func testNoSparesOrStrikes() {
+        var count: Int = 0
+        for _ in 1...20 {
+            let randNum = Int.random(in: 1..<5)
+            game?.roll(pins: randNum)
+            count = count + randNum
+        }
+        
+        XCTAssertEqual(game?.score(), count)
     }
 }
 
